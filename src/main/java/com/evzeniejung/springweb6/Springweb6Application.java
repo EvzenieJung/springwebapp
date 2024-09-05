812690810c1435e32d134d6a3c97781c2f581382
evzeniejung <evzenie.jung@gmail.com>
2024-09-05 16:21:26 +0200

    SpringWeb6 app Author and Book added

---
+++ src/main/java/com/evzeniejung/springweb6/Springweb6Application.java
@@ -0,0 +1,13 @@
+package com.evzeniejung.springweb6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb6Application.class, args);
+	}
+
+}
